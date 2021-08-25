cfd9e44ea7e06b25e72d770ef17ffd90ecfb0496
eudes.neto <eudes.neto@ambevtech.com.br>
2021-08-25 09:45:56 -0300

    Ex. 12 Javascript

---
+++ teams/start-tech/javascript/hamming/hamming.js
@@ -0,0 +1,23 @@
+export const compute = (esquerda, direita) => {
+  let hamming = 0;
+
+  if (esquerda === direita) {
+    return 0;
+
+  }else if (esquerda === '') {
+    throw new Error('left strand must not be empty');
+
+  }else if(direita === ''){
+    throw new Error('right strand must not be empty');
+
+  }else if(direita.length !== esquerda.length){
+    throw new Error('left and right strands must be of equal length');
+  } else{
+    for (let index = 0; index < esquerda.length; index++) {
+      if(esquerda[index] !== direita[index]){
+        hamming++
+      }
+    }
+    return hamming;
+  }
+};
